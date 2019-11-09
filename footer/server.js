1c92e774706f529ed9311f4c1048884d2a6b734a
daliaawad <awaddalia60@gmail.com>
2019-11-09 17:28:26 +0200

    SetUp footer

--- footer/server.js
+++ footer/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 var port = 3003;
-app.get("/", function(req, res) {
-  res.send("Hello World3");
-});
+
+
+
+
+
+app.use(express.static(__dirname + "/client/dist"));
+app.use(express.urlencoded());
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
