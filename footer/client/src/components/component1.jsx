a4c2c500edb5caa4fb5ce92d57deeee7854c41d5
daliaawad <awaddalia60@gmail.com>
2019-11-12 16:05:13 +0200

    Finish Footer

--- footer/client/src/components/component1.jsx
+++ footer/client/src/components/component1.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 
 
-class Component1 extends React.Component {
+class Explore extends React.Component {
     render () {
         return(
-            <div>Hello World</div>
+                <div id = "firstDiv">
+                    <ul>
+                        <li className="firstLi"><a href="/arts?ref=global-footer" >Arts</a></li>
+                        <li className="firstLi"><a href="/comics-illustration?ref=global-footer" >Comics & Illustration</a></li>
+                        <li className="firstLi"><a href="/design-tech?ref=global-footer" >Design & Tech</a></li>
+                        <li className="firstLi"><a href="/film?ref=global-footer" >Film</a></li>
+                        <li className="firstLi"><a href="/food-craft?ref=global-footer" >Food & Craft</a></li>
+                        <li className="firstLi"><a href="/games?ref=global-footer" >Games</a></li>
+                        <li className="firstLi"><a href="/music?ref=global-footer" >Music</a></li>
+                        <li className="firstLi"><a href="/publishing?ref=global-footer" >Publishing</a></li>
+                    </ul>
+                </div>
         )
     }
 }
 
 
 
-// const Component1 = () => (
-//     <div>Hello World</div>
-//   );
 
 
-export default Component1;
+export default Explore;
