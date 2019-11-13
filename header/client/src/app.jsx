a38181263bc6ecefcebd8c31567a645ecb839ba4
maysqunaibi <maysqunaibi2000@gmail.com>
2019-11-13 14:45:48 +0200

    header component

--- header/client/src/app.jsx
+++ header/client/src/app.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Headerbar from "./components/headerbar.jsx"
+import VideoSection from "./components/videoSection.jsx"
+import Info from "./components/info.jsx"
 var App = () => (
   <div>
-    react
-    <h1>react here</h1>
+    <Headerbar/>
+    <div id="title">
+      <h2>GoSun Flatware: Portable Utensils to End Single-Use Plastic</h2>
+      <p>
+        Reusable utensils that fit in your wallet, purse or pocket.</p>
+        </div>
+    {/* 
+    <table >
+  <tr> */}
+    
+    <VideoSection/>
+   <Info/>
+  {/* </tr>
+</table> */}
+    
+    
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("header"));
---
+++ header/client/src/components/info.jsx
@@ -0,0 +1,45 @@
+class Info extends React.Component{
+    render(){
+        return(
+
+            <div>
+                <div className="info">
+                    <div className="lineth" role="progressbar" aria-valuenow="6912" aria-valuemin="0" aria-valuemax="100">
+                    <div className="line h1 " ></div>
+										</div>
+                    <div className="">
+                        <div className="mb4 ml5">
+                            <div class="flex">
+                                <span class="pledeged">
+                                    <span class="">US$ 172,806</span></span>
+																		</div>
+																	
+                          <span class="type-font">
+                                <span class="">pledged of 
+																<span class="">US$ 2,500</span> 
+																goal</span>
+																</span>
+																</div>
+	
+																<div class="mb4 ml5">
+                                    <div class="type-2">
+																			<span>4,355</span>
+																		</div>
+                                    <span class="type-font">backers</span>
+																		</div>
+                            <div class="mb4 ml5">
+															<div>
+															<div>
+                        <span class="type-2">9</span>
+                        <span class="type-font">days to go</span>
+												</div>
+												</div>
+												</div>
+												</div>
+               
+								</div>
+            </div>
+        )
+    }
+}
+export default Info;
