d109664c05f685a6b65c5d891f5a76130640c146
YasmeenOthman <yasmeen.othman20@gmail.com>
2019-11-13 11:41:42 +0200

    sidebar module

--- FAQ/client/src/app.jsx
+++
@@ -1,9 +0,0 @@
-import Welcome from "./components/c1.jsx";
-import ReactDOM from "react-dom";
-import React from "react";
-class App extends React.Component {
-  render() {
-    return <Welcome />;
-  }
-}
-ReactDOM.render(<App />, document.getElementById("faq"));
