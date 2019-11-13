fb10247c9e42418c71b95267a611a98a489e36fd
mahanbahin <m.khamaiseh2@student.aaup.edu>
2019-11-13 10:54:11 +0200

    DB

--- campaign/client/src/app.jsx
+++ campaign/client/src/app.jsx
@@ -1,7 +1,12 @@
+import $ from "jquery";
 import About from "./components/campaign.jsx";
 import ReactDOM from "react-dom";
 import React from "react";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <About />;
   }
--- campaign/client/src/components/campaign.jsx
+++ campaign/client/src/components/campaign.jsx
@@ -1,169 +1,67 @@
+import $ from "jquery";
 import React from "react";
 class About extends React.Component {
-  render() {
-    return (
-      <div class="main">
-        <div class="px3">
-          <div class="mb3">
-            <h3 class="normal mb3 mb7-sm mobile-hide">About</h3>
-            <div class="mb3 mb10-sm mb3 js-risks"></div>
-            <div class="rte__content js-full-description responsive-media">
-              <div class="template asset" contenteditable="false">
-                <figure>
-                  <img
-                    class="fit"
-                    src="https://ksr-ugc.imgix.net/assets/026/961/835/1b358cf6811fa1a23eae271e53dd4a0a_original.png?ixlib=rb-2.1.0&w=680&fit=max&v=1571921442&auto=format&gif-q=50&lossless=true&s=14a295630969b4bbedc709ed8568074c"
-                  />
-                </figure>
-              </div>
-              <p>
-                Join <em>Book of Travels</em> and become part of a unique social
-                roleplaying experience that doesn’t hold your hand. Inspired by
-                genre classics, this is an online adventure that sets you adrift
-                in an intricate fairytale world... but it’s also an invitation
-                to roleplay without the restraints of linear quests and
-                plotlines. Feel at liberty to travel the free wilds and vivid
-                cities of the Braided Shore peninsula. Wander deep into the
-                layers of this hand-drawn world, stumble upon its hidden places
-                or unravel one of its many mysteries. There is no overarching
-                goal, no real beginning or end, and ultimately you are in charge
-                of shaping your own journey. It’s a land of elusive creatures,
-                lush natural beauty and pastoral calm... but one that has known
-                troubles and where shadows play their part.&nbsp;&nbsp;&nbsp;
-              </p>
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectData: []
+    };
 
-              <div
-                class="template oembed"
-                contenteditable="false"
-                data-href="https://www.youtube.com/watch?v=sJuGkGJG9zE&amp;t=1s&amp;frags=pl%2Cwn"
-              >
-                <iframe
-                  width="612"
-                  height="344"
-                  src="https://www.youtube.com/embed/sJuGkGJG9zE?start=1&amp;feature=oembed&amp;wmode=transparent"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen=""
-                ></iframe>
-              </div>
-              <h1 id="h:see-our-updates-for" class="page-anchor">
-                See our updates for the latest on lore and game play:
-              </h1>
-              <p>
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2664493"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #5:</strong>
-                </a>{" "}
-                All about knotspells •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2665790"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #8:</strong>
-                </a>{" "}
-                Go’Bad, draisine travel and healing tea •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2668347"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #9:</strong>
-                </a>{" "}
-                Design an island for <em>Book</em> <em>of</em> <em>Travels</em>{" "}
-                •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2674063"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #10:</strong>
-                </a>{" "}
-                Sefra lore •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2676682"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #11:</strong>
-                </a>{" "}
-                Game play lowdown •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2678748"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #12:</strong>
-                </a>{" "}
-                Mementos and artwork revealed. •{" "}
-                <a
-                  href="https://www.kickstarter.com/projects/bookoftravels/book-of-travels-a-serene-online-rpg/posts/2680122"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <strong>Update #13:</strong>
-                </a>{" "}
-                New stretch goals: Dreams + Pin-Pouch-Pebble minigame + Sharing
-                memories + Accessories + Pets.
-              </p>
-              <div
-                class="template asset"
-                contenteditable="false"
-                data-alt-text=""
-                data-caption=""
-                data-id="26961844"
-              >
-                <figure>
-                  <img
-                    class="fit"
-                    src="https://ksr-ugc.imgix.net/assets/026/961/844/9593b756a55b4c4bf4b9b3259ce3b093_original.png?ixlib=rb-2.1.0&amp;w=680&amp;fit=max&amp;v=1571921495&amp;auto=format&amp;gif-q=50&amp;lossless=true&amp;s=51bf79a856b05de640c079da717f8dfe"
-                  />
-                </figure>
-              </div>
-              <div
-                class="template asset"
-                contenteditable="false"
-                data-alt-text=""
-                data-caption=""
-                data-id="27005504"
-              >
-                <figure>
-                  <img
-                    alt=""
-                    class="fit"
-                    src="https://ksr-ugc.imgix.net/assets/027/005/504/dae5eff8b9585ef62e9675ac8d2ebbe9_original.png?ixlib=rb-2.1.0&amp;w=680&amp;fit=max&amp;v=1572289761&amp;auto=format&amp;gif-q=50&amp;lossless=true&amp;s=d68c9b9124aac56199665ffdaeb560a0"
-                  />
-                </figure>
-              </div>
-              <a
-                href="https://mightanddelight.com/rpg/ks/stretchgoals/11_stretchgoals_1-4_Unlocked_1200.png"
-                target="_blank"
-                rel="noopener"
-              >
-                <div
-                  class="template asset"
-                  contenteditable="false"
-                  data-alt-text=""
-                  data-caption=""
-                  data-id="27173677"
-                >
-                  <figure>
-                    <img
-                      alt=""
-                      class="fit"
-                      src="https://ksr-ugc.imgix.net/assets/027/173/677/9e01dfccb0d51d90930d60ac8329f7f6_original.png?ixlib=rb-2.1.0&amp;w=680&amp;fit=max&amp;v=1573483603&amp;auto=format&amp;gif-q=50&amp;lossless=true&amp;s=2846c304cc3d1bbd081398af1847b0cf"
-                    />
-                  </figure>
-                </div>
-              </a>
-            </div>
+    var that = this;
+    $.ajax({
+      type: "GET",
+      url: "/items",
+      success: function(collection) {
+        that.setState({
+          projectData: collection
+        });
+        console.log(collection);
+      },
+      error: function(request, status, error) {
+        console.log(error);
+      }
+    });
+  }
+
+  render() {
+    console.log(this.state.projectData);
+    if (this.state.projectData.length) {
+      return (
+        <div id="main">
+          <h3>About</h3>
+          <div>
+            <img src={this.props.data.projImg1} />
+            <br>{this.props.data.projDescription1}</br>
+            <img src={this.props.data.projImg2} />
+            <img src={this.props.data.projImg20} />
+            <br>{this.props.data.projDescription2}</br>
+            <img src={this.props.data.projImg3} />
+            <img src={this.props.data.projImg30} />
+            <img src={this.props.data.projImg31} />
+            <img src={this.props.data.projImg32} />
+            <br>{this.props.data.projDescription3}</br>
+            <img src={this.props.data.projImg4} />
+            <br>{this.props.data.projDescription4}</br>
+            <img src={this.props.data.projImg5} />
+            <img src={this.props.data.projImg50} />
+            <img src={this.props.data.projImg51} />
+            <img src={this.props.data.projImg52} />
+            <img src={this.props.data.projImg53} />
+            <br>{this.props.data.projDescription5}</br>
+            <br>{this.props.data.projDescription50}</br>
+            <img src={this.props.data.projImg6} />
+            <img src={this.props.data.projImg60} />
+            <br>{this.props.data.projDescription6}</br>
+            <br>{this.props.data.projDescription60}</br>
+            <br>{this.props.data.projDescription61}</br>
+            <br>{this.props.data.projDescription62}</br>
+            <br>{this.props.data.projDescription63}</br>
+            <img src={this.props.data.projImg7} />
           </div>
+          ))
         </div>
-      </div>
-    );
+      );
+    } else null;
   }
 }
 
