64b1a62b80357f0344c654582a682f6a0668dcf1
YasmeenOthman <yasmeen.othman20@gmail.com>
2019-11-13 12:14:23 +0200

    sidebar

---
+++ sidebar/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
+module.exports = {
+  entry: __dirname + "/client/src/app.jsx",
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/client/dist"
+  }
+};
