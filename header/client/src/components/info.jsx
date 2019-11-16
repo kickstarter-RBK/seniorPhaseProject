bab07415ea98471b1dd6d1d6e44cff482f501bd2
maysqunaibi <maysqunaibi2000@gmail.com>
2019-11-16 12:08:28 +0200

    edit header

--- header/client/src/components/info.jsx
+++ header/client/src/components/info.jsx
@@ -1,73 +1,91 @@
 class Info extends React.Component{
-    render(){
-        return(
+  render(){
+      return(
 
-            <div>
-                <div className="info">
-                    <div className="lineth" role="progressbar" aria-valuenow="6912" aria-valuemin="0" aria-valuemax="100">
-                    <div className="line h1 " ></div>
-										</div>
-                    <div className="">
-                        <div className="mb4 ml5">
-                            <div class="flex">
-                                <span class="pledeged">
-                                    <span class="">US$ 172,806</span></span>
-																		</div>
-																	
-                          <span class="type-font">
-                                <span class="">pledged of 
-																<span class="">US$ 2,500</span> 
-																goal</span>
-																</span>
-																</div>
-	
-																<div class="mb4 ml5">
-                                    <div class="type-2">
-																			<span>4,355</span>
-																		</div>
-                                    <span class="type-font">backers</span>
-																		</div>
-                            <div class="mb4 ml5">
-															<div>
-															<div>
-                        <span class="type-2">9</span>
-                        <span class="type-font">days to go</span>
-												</div>
-												</div>
-												</div>
-												</div>
-                {/* <div class="mb4">
-                  <a  className="back-project-buttonClass" href="" > Back this project </a>
-                <div className="flex mb3">
-                  <div class="block basis50p grow-1 w100p">
-                    <div>
-                      <div>
-                  <button type="button" class="remindBtn">
-                    <svg class="svg-icon__icon--bookmark icon-medium mr1 relative t-1_6" aria-hidden="true">
-                      </svg>
-                     Remind me
-                      </button>
+          <div>
+              <div className="info">
+                  <div className="lineth" role="progressbar" aria-valuenow="6912" aria-valuemin="0" aria-valuemax="100">
+                  <div className="line h1 " ></div>
+                  </div>
+                  <div className="">
+                      <div className="mb4 ml5">
+                          <div class="flex">
+                              <span class="pledeged">
+                                  <span class="">US$ 219,806</span></span>
+                                  </div>		
+                        <span class="type-font">
+                              <span class="">pledged of 
+                              <span class="">US$ 5,600</span> 
+                              goal</span>
+                              </span>
+                              </div>
+
+                              <div class="mb4 ml5">
+                                  <div class="type-2">
+                                    <span>4,355</span>
+                                  </div>
+                                  <span class="type-font">backers</span>
+                                  </div>
+                          <div class="mb4 ml5">
+                            <div>
+                            <div>
+                      <span class="type-2">6</span>
+                      <span class="type-font">days to go</span>
                       </div>
-                      <div>
-                        </div>
-                        </div>
-                        </div>
-                        <div class="basis50p block-sm flex grow-1 items-center justify-center react_share_icons w100p"><div class="w100p"><div class="flex items-center justify-between justify-end-lg"><div class="inline-block mx4">
-                          <a href="" class="keyboard-focusable">
-                        <svg class="svg-icon__icon--facebook icon-20 fill-facebook-blue" aria-hidden="true"></svg><span class="hide">Share</span></a></div><div class="inline-block mr4">
-                          <a href="https://twitter.com/intent/tweet?text=Check%20out%20GoSun%20Flatware%3A%20Portable%20Utensils%20to%20End%20Single-Use%20Plastic%20by%20Patrick%20Sherwin%20on%20%40Kickstarter&amp;url=https://www.kickstarter.com/projects/patricksherwin/gosun-flatware-reusable-utensils-to-stop-single-use-plastic?ref=project_tweet" class="keyboard-focusable">
-                          <svg class="svg-icon__icon--twitter icon-20 fill-twitter-blue" aria-hidden="true">
-                          </svg><span class="hide">Tweet</span></a></div><div class="inline-block mr4"><a href="mailto:?subject=GoSun%20Flatware:%20Portable%20Utensils%20to%20End%20Single-Use%20Plastic%20on%20Kickstarter&amp;body=http://kck.st/2PcPqXO" class="keyboard-focusable">
-                            <svg class="svg-icon__icon--mail icon-20 fill-dark-grey-500" aria-hidden="true"></svg><span class="hide">Mail</span></a></div><div class="inline-block"><a href="#" class="keyboard-focusable">
-                              <svg class="svg-icon__embed icon-20 fill-dark-grey-500" aria-hidden="true"></svg><span class="hide">Embed</span>
-                </a>
-                </div></div>
-                </div></div>
-                </div>
-                </div> */}
-								</div>
-            </div>
-        )
-    }
+                      </div>
+                      </div>
+                      </div>
+              <div class="mb4">
+                <a  className="back-project-buttonClass" href=""> Back this project </a>
+
+              <div className="flex mb4">
+                 <button type="button" class="remindBtn">
+                   Remind me
+                    </button>
+                   
+                    <div > 
+                    <div className="rmicons">
+                    <div class="inline-block mr3">
+                    <a href="https://www.kickstarter.com/projects/patricksherwin/gosun-flatware-reusable-utensils-to-stop-single-use-plastic?ref=project_facebook">
+                        <svg id="icon--facebook" viewBox="0 0 60 60">
+                          <path d="M54 30.147C54 16.81 43.255 6 30 6S6 16.81 6 30.147C6 42.2 14.776 52.19 26.25 54V37.127h-6.094v-6.98h6.094v-5.32c0-6.052 3.583-9.395 9.065-9.395 2.626 0 5.373.472 5.373.472v5.942H37.66c-2.98 0-3.91 1.862-3.91 3.77v4.53h6.656l-1.064 6.98H33.75V54C45.224 52.19 54 42.2 54 30.147" fill-rule="evenodd"></path>
+                            </svg>
+                            </a>
+                      </div>
+                      <div class="inline-block mr4">
+                        <a href="https://twitter.com/intent/tweet?text=Check%20out%20GoSun%20Flatware%3A%20Portable%20Utensils%20to%20End%20Single-Use%20Plastic%20by%20Patrick%20Sherwin%20on%20%40Kickstarter&amp;url=https://www.kickstarter.com/projects/patricksherwin/gosun-flatware-reusable-utensils-to-stop-single-use-plastic?ref=project_tweet" >
+                          <svg id="icon--twitter" viewBox="0 0 60 60">
+                            <g fill-rule="evenodd">
+                              <path d="M4.11 46.748c.775.12 1.62.178 2.533.178 2.364 0 4.678-.458 6.942-1.37 2.264-.912 4.35-2.136 6.258-3.674-2.28-.014-4.336-.72-6.17-2.114-1.832-1.394-3.078-3.154-3.736-5.284.71.136 1.368.204 1.976.204.98 0 1.926-.128 2.838-.382-2.45-.488-4.485-1.708-6.105-3.66-1.622-1.95-2.434-4.21-2.434-6.776v-.128c1.523.826 3.127 1.266 4.815 1.316-1.468-.978-2.63-2.246-3.484-3.8-.85-1.552-1.277-3.224-1.277-5.016 0-1.74.48-3.462 1.444-5.17 2.668 3.244 5.894 5.816 9.678 7.718 3.784 1.9 7.864 2.968 12.238 3.204-.152-.66-.228-1.478-.228-2.458 0-2.94 1.036-5.444 3.104-7.512 2.067-2.07 4.573-3.104 7.51-3.104 3.025 0 5.635 1.116 7.83 3.344 2.417-.49 4.655-1.352 6.715-2.584-.81 2.566-2.372 4.534-4.688 5.904 2.112-.238 4.156-.802 6.132-1.7-1.42 2.148-3.183 3.988-5.297 5.524v1.394c0 2.366-.3 4.742-.898 7.134-.6 2.39-1.462 4.712-2.584 6.966-1.123 2.256-2.563 4.364-4.32 6.322-1.757 1.96-3.725 3.67-5.903 5.132-2.18 1.46-4.692 2.612-7.538 3.458-2.847.844-5.85 1.266-9.01 1.266-6.4 0-11.847-1.444-16.34-4.332z"></path>
+                              </g>
+                              </svg>
+                          </a></div>
+                          <div class="inline-block mr4">
+                            <a href="mailto:?subject=GoSun%20Flatware:%20Portable%20Utensils%20to%20End%20Single-Use%20Plastic%20on%20Kickstarter&amp;body=http://kck.st/2PcPqXO" >
+                            <svg id="icon--mail" viewBox="0 0 60 60">
+                                  <g fill-rule="evenodd"><path d="M30 31.39L8.138 12.88c-.356-.3-.142-.88.322-.88h43.08c.464 0 .676.58.322.88L30 31.39zM4.823 15.317l16.75 14.176-16.74 15.064c-.32.29-.834.06-.834-.37v-28.49c0-.424.497-.656.823-.38zm50.355 0c.324-.276.822-.044.822.38v28.49c0 .43-.512.66-.834.37L38.43 29.496l16.748-14.178zm-19.83 16.786L52.044 47.13c.34.306.122.87-.336.87H8.292c-.458 0-.674-.564-.334-.87l16.696-15.028 3.428 2.902c.554.462 1.232.694 1.914.694.686 0 1.372-.234 1.936-.704l3.416-2.89z">
+                                    </path></g>
+                                  </svg>
+                              </a></div>
+                              <div class="inline-block">
+                                <a href="#" >
+                                <svg id="icon--mail" viewBox="0 0 26 16">
+                                  <path d="M25.33 7.473L18.005 2.32c-.213-.152-.432-.074-.432.186V4.4c0 .302.016.38.186.496l4.21 2.962v.002l.083.06-.084.06v.003l-4.21 2.962c-.17.115-.187.193-.187.494v1.894c0 .26.22.336.432.186l7.325-5.152c.23-.162.342-.307.342-.448 0-.142-.113-.285-.342-.447zM13.81 0c-.134 0-.19.074-.214.176L11.47 8h.002l-2.124 7.818c-.037.13-.006.182.115.182h1.995c.135 0 .188-.074.213-.176h.003L13.797 8 15.92.18c.036-.13.005-.182-.116-.182H13.81zM7.666 2.32L.342 7.472c-.23.16-.342.305-.342.448 0 .142.113.286.342.446l7.325 5.155c.213.15.432.075.432-.187v-1.895c0-.3-.017-.377-.187-.493L3.7 7.98h.003l-.084-.06.083-.062-.002-.002 4.213-2.962c.17-.115.186-.193.186-.494V2.507c0-.262-.22-.338-.433-.188z" fill-rule="evenodd"></path>
+                                    </svg>
+                                    </a></div>
+                                    <div>
+                                     <p>This project will only be funded if it reaches its goal by Sat, November 23 2019 6:59 AM UTC +02:00.</p>
+                                    </div>
+                                    </div>
+                                    
+                      </div>
+                      
+                      
+              </div>
+              </div>
+              </div>
+          </div>
+      )
+  }
 }
 export default Info;
